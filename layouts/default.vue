af4c59e2ccc8e607b49f0c185d6d8711916b9478
gurva <gurvan.seveno@gmail.com>
2021-07-01 21:06:28 +0200

    Changed things

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,139 +1,141 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      v-show="$auth.loggedIn"
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      mini-variant-width="6em"
-      fixed
-      color="secondary"
-      elevation="0"
-      app
-    >
-      <v-list
-        rounded
-        class="my-4"
+  <client-only>
+    <v-app>
+      <v-navigation-drawer
+        v-show="$auth.loggedIn"
+        v-model="drawer"
+        :mini-variant="miniVariant"
+        mini-variant-width="6em"
+        fixed
+        color="secondary"
+        elevation="0"
+        app
       >
-        <img
-          width="48"
-          height="64"
-          class="ml-4 mt-8 mb-8"
-          alt="Login"
-          :src="logo"
+        <v-list
+          rounded
+          class="my-4"
         >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          exact-active-class="selected"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-app-bar
-      v-show="$auth.loggedIn"
-      class="px-4 mb-8 mt-8"
-      style="transition: color 0s"
-      fixed
-      flat
-      absolute
-      app
-      color="background"
-    >
-      <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <v-spacer />
-      <v-btn
-        class="mr-2"
-        icon
-        @click="switchTheme()"
+          <img
+            width="48"
+            height="64"
+            class="ml-4 mt-8 mb-8"
+            alt="Login"
+            :src="logo"
+          >
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            exact-active-class="selected"
+            :to="item.to"
+            router
+            exact
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <v-container>
+          <nuxt />
+        </v-container>
+      </v-main>
+      <v-app-bar
+        v-show="$auth.loggedIn"
+        class="px-4 mb-8 mt-8"
+        style="transition: color 0s"
+        fixed
+        flat
+        absolute
+        app
+        color="background"
       >
-        <v-icon>
-          mdi-brightness-4
-        </v-icon>
-      </v-btn>
-      <template class="d-block justify-end align-end">
-        <v-menu open-on-hover bottom offset-y max-height="300">
-          <template #activator="{ on, attrs }">
-            <v-badge
-              :content="notifications.length"
-              style="border-color: var(v--background-color)"
-              :value="notifications.length"
-              class="mr-4"
-              color="green"
-              bordered
-              overlap
-            >
-              <v-icon
-                v-bind="attrs"
-                v-on="on"
-              >
-                mdi-bell
-              </v-icon>
-            </v-badge>
-          </template>
-          <v-list rounded color="secondary" max-width="375">
-            <template v-for="(item, a) in notifications">
-              <notification
-                :key="a"
-                :notification="item"
-              />
-            </template>
-            <v-list-item to="/notifications" class="d-flex justify-center">
-              <v-list-item-content>
-                <v-list-item-title>
-                  En voir plus
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-list color="background" dense rounded class="mr-8">
-          <v-menu open-on-hover bottom offset-y>
+        <v-app-bar-nav-icon @click="drawer = !drawer" />
+        <v-spacer />
+        <v-btn
+          class="mr-2"
+          icon
+          @click="switchTheme()"
+        >
+          <v-icon>
+            mdi-brightness-4
+          </v-icon>
+        </v-btn>
+        <template class="d-block justify-end align-end">
+          <v-menu open-on-hover bottom offset-y max-height="300">
             <template #activator="{ on, attrs }">
-              <v-list-item
-                v-if="$auth.loggedIn"
-                v-bind="attrs"
-                v-on="on"
+              <v-badge
+                :content="notifications.length"
+                style="border-color: var(v--background-color)"
+                :value="notifications.length"
+                class="mr-4"
+                color="green"
+                bordered
+                overlap
               >
-                <v-list-item-avatar style="border: solid var(--v-primary-base) 2px">
-                  <v-img :src="$auth.user.avatar" />
-                </v-list-item-avatar>
-                <v-list-item-title v-if="on">
-                  <h3>{{ $auth.user.firstName }}</h3>
-                </v-list-item-title>
-              </v-list-item>
+                <v-icon
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  mdi-bell
+                </v-icon>
+              </v-badge>
             </template>
-            <v-list rounded color="background">
-              <v-list-item
-                nuxt
-                to="/profil"
-              >
-                <v-list-item-title>Mon compte</v-list-item-title>
-              </v-list-item>
-              <v-list-item
-                @click="$auth.logout()"
-              >
-                <v-list-item-title>Deconnexion</v-list-item-title>
+            <v-list rounded color="secondary" max-width="375">
+              <template v-for="(item, a) in notifications">
+                <notification
+                  :key="a"
+                  :notification="item"
+                />
+              </template>
+              <v-list-item to="/notifications" class="d-flex justify-center">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    En voir plus
+                  </v-list-item-title>
+                </v-list-item-content>
               </v-list-item>
             </v-list>
           </v-menu>
-        </v-list>
-      </template>
-    </v-app-bar>
-  </v-app>
+          <v-list color="background" dense rounded class="mr-8">
+            <v-menu open-on-hover bottom offset-y>
+              <template #activator="{ on, attrs }">
+                <v-list-item
+                  v-if="$auth.loggedIn"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-list-item-avatar style="border: solid var(--v-primary-base) 2px">
+                    <v-img :src="$auth.user.avatar" />
+                  </v-list-item-avatar>
+                  <v-list-item-title v-if="on">
+                    <h3>{{ $auth.user.firstName }}</h3>
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+              <v-list rounded color="background">
+                <v-list-item
+                  nuxt
+                  to="/profil"
+                >
+                  <v-list-item-title>Mon compte</v-list-item-title>
+                </v-list-item>
+                <v-list-item
+                  @click="$auth.logout()"
+                >
+                  <v-list-item-title>Deconnexion</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-list>
+        </template>
+      </v-app-bar>
+    </v-app>
+  </client-only>
 </template>
 
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Technique',
-    title: 'Delivery',
+    title: 'Technique',
     htmlAttrs: {
       lang: 'fr'
     },
